d66307bf732e32d139eef982602d06dc9e192c79
Hasan Hüseyin Semiz <hhsem49@gmail.com>
2023-10-07 11:51:04 +0200

    edited

--- src/test/java/com/example/services/GetAllProductsList.java
+++ src/test/java/com/example/services/GetAllProductsList.java
@@ -1,2 +1,26 @@
-package com.example.services;public class getAllProductsList {
+package com.example.services;
+
+import com.example.utilities.Globals;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.Assert;
+
+import java.util.List;
+import java.util.Map;
+
+public class GetAllProductsList extends Globals {
+    public void getAllProductsList(){
+        response = RestAssured.given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/api/productsList");
+    }
+
+    public void validateThatAllProductsAreListed(){
+        Assert.assertEquals(200, response.statusCode());
+
+        List<Map> products = response.jsonPath().getList("products");
+        Assert.assertEquals(34, products.size());
+    }
 }
