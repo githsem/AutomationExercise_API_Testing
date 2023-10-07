8b6c4ea19b72dbda6ce0cdbe44fb090f852cfd0f
Hasan Hüseyin Semiz <hhsem49@gmail.com>
2023-10-07 11:51:12 +0200

    edited

--- src/test/java/com/example/step_defs/GetAllProductsList_stepDefs.java
+++ src/test/java/com/example/step_defs/GetAllProductsList_stepDefs.java
@@ -1,2 +1,22 @@
-package com.example.step_defs;public class getAllProductsList_stepDefs {
+package com.example.step_defs;
+
+import com.example.services.GetAllProductsList;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+public class GetAllProductsList_stepDefs {
+
+    GetAllProductsList getAllProductsList = new GetAllProductsList();
+
+    @When("Get All Products List")
+    public void get_all_products_list() {
+        getAllProductsList.getAllProductsList();
+    }
+
+    @Then("Validate that all products are listed")
+    public void validate_that_all_products_are_listed() {
+        getAllProductsList.validateThatAllProductsAreListed();
+    }
+
 }
