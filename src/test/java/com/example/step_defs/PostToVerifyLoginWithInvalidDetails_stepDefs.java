19ec57def7ead0ddddad6c35ec3ec0a129cb405e
Bilal Saglam <billall.saglam@gmail.com>
2023-10-09 15:35:01 +0200

    api10

---
+++ src/test/java/com/example/step_defs/PostToVerifyLoginWithInvalidDetails_stepDefs.java
@@ -0,0 +1,21 @@
+package com.example.step_defs;
+
+import com.example.services.PostToVerifyLoginWithInvalidDetails;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+
+
+public class PostToVerifyLoginWithInvalidDetails_stepDefs {
+
+    PostToVerifyLoginWithInvalidDetails postToVerifyLoginWithInvalidDetails=new PostToVerifyLoginWithInvalidDetails();
+    @When("User writes invalid email and password")
+    public void user_writes_invalid_email_and_password() {
+        postToVerifyLoginWithInvalidDetails.postToVerifyLoginWithInvalidDetails();
+    }
+    @Then("Validate that invalid result")
+    public void validate_that_invalid_result() {
+        postToVerifyLoginWithInvalidDetails.validateLoginWithInvalidDetails();
+    }
+
+}
